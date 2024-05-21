9524de915d63b8aa359acdaafbb73609aa249019
Digvijay Patil <160723513+Digvijay548@users.noreply.github.com>
2024-05-21 16:44:07 +0530

    Add files via upload

--- src/Layout.js
+++ src/Layout.js
@@ -9,14 +9,14 @@ import './Layout.css'; // Import CSS file
 
 function Layout() {
   return (
-    
-      <div className="layout-container">
-        <Header />
-        <div className="content">
-          <Outlet />
-        </div>
-        <Footer className="footer"/>
+
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <div className="flex-grow mt-0">
+        <Outlet />
       </div>
+      <Footer/>
+    </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,57 +1,62 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext } from 'react';
 import ReactDOM from 'react-dom/client';
 import Layout from './Layout';
 import Home from './Pages/Home';
 import Contact from './Pages/Contact';
 import About from './Pages/About';
-import reportWebVitals from './reportWebVitals';
 import PersonalPage from './Pages/PersonalPage';
 import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom';
 import ComapnyPage from './Pages/CompanyPage';
 import Login from './Pages/Login';
 import NewUser from './Pages/NewUser';
 import ForgotPassword from './Pages/ForgotPassword';
-import Github,{githubInfoLoader} from './Pages/Github';
+import Github, { githubInfoLoader } from './Pages/Github';
+import './index.css';
+import { Provider } from 'react-redux';
+import store from './store/store.js';
+import FileUpload from './Pages/FileUpload.js';
+import AllPost from './Pages/AllPost.js';
 
 // Step 1: Create the Context
- export const UserContext = createContext();
+export const UserContext = createContext();
 
 // Step 2: Provide the Context
 const App = () => {
-  const [user, setUser] = useState("null"); // Example: state to hold user data
-  const [IsUserLoggedIn, setIsUserLoggedIn] = useState(false);
 
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<Layout />}>
+        
         <Route path='' element={<Home />} />
         <Route path='/login' element={<Login />} />
         <Route path='/forget-password' element={<ForgotPassword />} />
         <Route path='/new-user' element={<NewUser />} />
         <Route path='/about' element={<About />} />
-        <Route path='about/ComapnyPage' element={<ComapnyPage />} /> {/* Corrected typo */}
-        <Route path='about/PersonalPage' element={<PersonalPage />} /> {/* Corrected typo */}
+        <Route path='about/ComapnyPage' element={<ComapnyPage />} /> {/*Corrected typo*/}
+        <Route path='about/PersonalPage' element={<PersonalPage />} /> {/*Corrected typo*/}
         <Route path='contact' element={<Contact />} />
         <Route path='contact' element={<Contact />} />
-        <Route loader={githubInfoLoader} path='github' element={<Github />}/>
-       
+        <Route loader={githubInfoLoader} path='github' element={<Github />} />
+        <Route  path='/FileUpload' element={<FileUpload />} />
+        <Route path='/AllPost' element={<AllPost />} />
+
       </Route>
     )
   );
 
   return (
     // Step 2: Provide the Context
-    <UserContext.Provider value={{ user, setUser ,IsUserLoggedIn,setIsUserLoggedIn}}>
+    <Provider store={store}>
       <React.StrictMode>
         <RouterProvider router={router} />
       </React.StrictMode>
-    </UserContext.Provider>
+    </Provider>
+
   );
 };
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-reportWebVitals();
 
 export default App;
