11bad6953d556687720dd9b440a1410dc397344b
Digvijay Patil <160723513+Digvijay548@users.noreply.github.com>
2024-05-22 13:19:23 +0530

    Layout Class

--- src/Layout.js
+++ src/Layout.js
@@ -1,21 +1,16 @@
-// Layout.js
-
 import React from 'react';
 import Header from './Pages/Header';
 import Footer from './Pages/Footer';
 import { Outlet } from 'react-router-dom';
 import './Layout.css'; // Import CSS file
 
-
 function Layout() {
   return (
-
     <div className="flex flex-col min-h-screen">
       <Header />
-      <div className="flex-grow mt-0">
-        <Outlet />
-      </div>
-      <Footer/>
+      <Outlet />
+
+      <Footer />
     </div>
   );
 }
